e2b5560768ef350bd3c6e1ec6721bd5b9d2d9e2b
Andras Kadar <kadarandrass@gmail.com>
2012-02-16 15:14:50 +0100

    File added: Tools/latex.java

---
+++ Tools/latex.java
@@ -0,0 +1,127 @@
+import com.sun.javadoc.*;
+import java.util.Arrays;
+
+public class latex extends Doclet {
+
+	public static boolean start(RootDoc root) {
+		System.out.println("==== CUT HERE =====");
+
+		ClassDoc[] classes = root.classes();
+		Arrays.sort(classes);
+		for (ClassDoc cd : classes) {
+			System.out.println("\\oszt{" + cd.name() + "}");
+			if (cd.isInterface()) {
+				System.out.println("Interfész.");
+			} else if (cd.isAbstract()) {
+				System.out.println("Absztrakt osztály.");
+			}
+
+			System.out.println("\\begin{description}");
+			System.out.println("\\item[Felelősség]");
+			System.out.println(cd.commentText().replace('\n', ' '));
+			if (cd.commentText().length() < 5) {
+				System.out.println(" % TODO");
+			}
+			System.out.println("\\item[Ősosztályok] " + getSupers(cd) + ".");
+			System.out.println("\\item[Interfészek] " + getIfaces(cd));
+			if (!cd.isInterface()) {
+				System.out.println("\\item[Attribútumok]$\\ $\n\\begin{description}");
+				printFields(cd.fields());
+				System.out.println("\\end{description}");
+			}
+			System.out.println("\\item[Metódusok]$\\ $\n\\begin{description}");
+			printMembers(cd.methods());
+			System.out.println("\\end{description}");
+			System.out.println("\\end{description}");
+			System.out.println();
+		}
+		System.out.println("==== END CUT HERE =====");
+		return true;
+	}
+
+	private static String getIfaces(ClassDoc cd) {
+		ClassDoc[] ifs = cd.interfaces();
+		String s = null;
+		Arrays.sort(ifs);
+		for (ClassDoc iface : ifs) {
+			s = (s == null ? "" : s + ", ") + iface.name();
+		}
+		return s == null ? "(nincs)" : (s + ".");
+	}
+
+	private static String getSupers(ClassDoc cl) {
+		ClassDoc sup = cl.superclass();
+		if (sup != null) {
+			return getSupers(sup) + " $\\rightarrow{}$ " + cl.name();
+		} else {
+			return cl.name();
+		}
+
+	}
+
+	private static void printFields(FieldDoc[] mems) {
+		Arrays.sort(mems);
+		for (FieldDoc mem : mems) {
+			System.out.print("\t\\item[\\texttt{" + mem.modifiers() + " "
+				+ getTypeName(mem.type()) + " " + mem.name().replace("_", "\\_") + "}]");
+			System.out.println(mem.commentText().replace('\n', ' '));
+			if (mem.commentText().length() < 5) {
+				System.out.println(" % TODO");
+			}
+		}
+		if (mems.length == 0) {
+			System.out.println("\\item[] (nincs)");
+		}
+	}
+
+	static void printMembers(MethodDoc[] mems) {
+		Arrays.sort(mems);
+		for (MethodDoc mem : mems) {
+			if (mem.name().compareTo("toString") != 0) {
+				System.out.print("\t\\item[\\texttt{" + mem.modifiers() + " "
+					+ getTypeName(mem.returnType()) + " "
+					+ mem.name().replace("_", "\\_") + "(" + getParams(mem).replace("_", "\\_") + ")}] \\hfill \\\\");
+				System.out.println(mem.commentText().replace('\n', ' '));
+				if (mem.commentText().length() < 5) {
+					System.out.println(" % TODO");
+				}
+			}
+		}
+		if (mems.length == 0) {
+			System.out.println("\\item[] (nincs)");
+		}
+	}
+
+	static private String getTypeName(Type t) {
+		String name = t.simpleTypeName();
+		String par = "";
+		try {
+			TypeVariable[] args = t.asClassDoc().typeParameters();
+			for (TypeVariable arg : args) {
+				if (par.length() > 0) {
+					par += ", ";
+				}
+				par += getTypeName(arg);
+			}
+		} catch (Exception e) {
+			par = "";
+		}
+		if (par.length() > 0) {
+			name += "<" + par + ">";
+		}
+		name += t.dimension();
+		return name;
+	}
+
+	private static String getParams(MethodDoc mem) {
+		Parameter[] pars = mem.parameters();
+		String ret = "";
+		for (Parameter p : pars) {
+			if (ret.length() > 0) {
+				ret += ", ";
+			}
+			ret += getTypeName(p.type()) + " " + p.name();
+		}
+		return ret.replace("_", "\\_");
+	}
+}
