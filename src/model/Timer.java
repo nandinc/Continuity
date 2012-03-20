5a973ea5568602c7188964e936db9a066efe7036
erenon <erenon2@gmail.com>
2012-03-20 20:49:08 +0100

    Timer initial implementation

--- src/model/Timer.java
+++ src/model/Timer.java
@@ -1,5 +1,7 @@
 package model;
 
+import java.util.TimerTask;
+
 /**
  * Az idő múlását nyilvántartó objektum. 
  * Elsősorban Stickman esését szabályozhatjuk vele, de a
@@ -14,6 +16,26 @@ public class Timer {
 	 * Az eseménykezelő csatorna,
 	 * melyen jelzi az idő múlását.
 	 */
-    PubSub pubsub;
+    private PubSub pubSub;
+    
+    private java.util.Timer timer;
+    
+    public void setPubSub(PubSub pubSub) {
+    	this.pubSub = pubSub;
+    }
+
+	public void start() {
+		// true: runs as a daemon
+    	// @see: http://docs.oracle.com/javase/1.4.2/docs/api/java/util/Timer.html#Timer(boolean)
+		// timer couldn't be initialized in the constructor, because it's not restartable, see docs.
+    	timer = new java.util.Timer(true);
+    	timer.schedule(new TimerTask() {
+			
+			@Override
+			public void run() {
+				pubSub.emit("tick", null);
+			}
+		}, 100, 100);
+	}
 
 }
