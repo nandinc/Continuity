90bcadf0a0b38eaec7daceead3b18e483d404783
erenon <erenon2@gmail.com>
2012-03-20 20:50:03 +0100

    Domain specific custom exceptions

---
+++ src/model/exception/MapErrorException.java
@@ -0,0 +1,21 @@
+package model.exception;
+
+public class MapErrorException extends Exception {
+	private static final long serialVersionUID = 1L;
+	
+	public MapErrorException() {
+		super();
+	}
+	
+	public MapErrorException(String message) {
+		super(message);
+	}
+	
+	public MapErrorException(String message, Throwable cause) {
+		super(message, cause);
+	}
+	
+	public MapErrorException(Throwable cause) {
+		super(cause);
+	}
+}
---
+++ src/model/exception/MapNotFoundException.java
@@ -0,0 +1,5 @@
+package model.exception;
+
+public class MapNotFoundException extends Exception {
+	private static final long serialVersionUID = 1L;
+}
