4d373018390f586d87d874b962cb764c8e836512
erenon <erenon2@gmail.com>
2012-03-20 20:47:13 +0100

    PubSub logic implemented

--- src/model/PubSub.java
+++ src/model/PubSub.java
@@ -1,11 +1,15 @@
 package model;
 
-import java.util.*;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
 
 /**
  * Üzenetközvetítő csatorna, mely a Publish/Subscribe mintát valósítja meg.
  * 
  * @responsibility Üzenetközvetítő osztály, mely feliratkozásokat tart számon, és ha valakitől eseményt kap, arról értesíti az arra feliratkozottakat.
+ * @file PubSub osztály
  */
 public class PubSub {
 
@@ -16,7 +20,12 @@ public class PubSub {
      * @param data Az eseményhez tartozó paraméter.
      */
     public void emit(String eventName, Object data) {
-        throw new UnsupportedOperationException();
+    	if (subscribers.containsKey(eventName)) {
+	    	List<Subscriber> eventSubs = subscribers.get(eventName);
+	    	for (Subscriber subscriber : eventSubs) {
+				subscriber.eventEmitted(eventName, data);
+			}
+    	}
     }
 
     /**
@@ -26,12 +35,16 @@ public class PubSub {
      * @param callback Az esemény bekövetkeztekor meghívandó eseménykezelő
      */
     public void on(String eventName, Subscriber callback) {
-        throw new UnsupportedOperationException();
+        if (!subscribers.containsKey(eventName)) {
+        	subscribers.put(eventName, new LinkedList<Subscriber>());
+        }
+        
+        subscribers.get(eventName).add(callback);
     }
 
     /**
      * Az eseményekre feliratkozott eseménykezelőket tárolja.
      */
-    private Collection<Subscriber> subscribers;
+    private Map<String, List<Subscriber>> subscribers = new HashMap<String, List<Subscriber>>();
 
 }
