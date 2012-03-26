82e6fe75f70ce1fae39b57523bd632485a08e462
erenon <erenon2@gmail.com>
2012-03-26 18:55:29 +0200

    Map: FrameIterator added

--- src/model/Map.java
+++ src/model/Map.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 
@@ -12,14 +13,14 @@ import java.util.List;
  */
 public class Map {
 
-	/**
-	 * A pályához tartozó kereteket tárolja. A Map osztály
-	 * meg tudja állapítani a keretek közötti szomszédossági
-	 * viszonyokat a gyűjtemény alapján.
-	 * 
-	 * A 2 dimenziós lista először az x eltolással, 
-	 * majd az y eltolással indexelhető.
-	 */
+    /**
+     * A pályához tartozó kereteket tárolja. A Map osztály
+     * meg tudja állapítani a keretek közötti szomszédossági
+     * viszonyokat a gyűjtemény alapján.
+     * 
+     * A 2 dimenziós lista először az x eltolással, 
+     * majd az y eltolással indexelhető.
+     */
     protected List<List<Frame>> frames = new ArrayList<List<Frame>>();
 
     /**
@@ -32,36 +33,36 @@ public class Map {
      */
     public void addItem(FrameItem item) {
         // determine containing frame
-    	int frameCoordX = item.getArea().getX() / Frame.FRAME_WIDTH;
-    	int frameCoordY = item.getArea().getY() / Frame.FRAME_HEIGHT;
-    	
-    	// add new frame columns if needed
-    	if (frames.size() <= frameCoordX) {
-    		for (int currentSize = frames.size(); currentSize <= frameCoordX; currentSize++) {
-				frames.add(new ArrayList<Frame>());
-			}
-    	}
-    	
-    	List<Frame> column = frames.get(frameCoordX);
-    	// add new frames if needed
-    	if (column.size() <= frameCoordY) {
-    		for (int currentSize = column.size(); currentSize <= frameCoordY; currentSize++) {
-    			Frame frame = new Frame();
-    			frame.setMap(this);
-    			column.add(frame);
-    		}
-    	}
-    	
-    	// set item position relative to frame
-    	int itemOffsetX = item.getArea().getX() % Frame.FRAME_WIDTH;
-    	int itemOffsetY = item.getArea().getY() % Frame.FRAME_HEIGHT;
-    	
-    	item.getArea().setX(itemOffsetX);
-    	item.getArea().setY(itemOffsetY);
-    	
-    	// finally add item to the containing frame
-    	Frame containingFrame = column.get(frameCoordY);
-    	containingFrame.addItem(item);
+        int frameCoordX = item.getArea().getX() / Frame.FRAME_WIDTH;
+        int frameCoordY = item.getArea().getY() / Frame.FRAME_HEIGHT;
+
+        // add new frame columns if needed
+        if (frames.size() <= frameCoordX) {
+            for (int currentSize = frames.size(); currentSize <= frameCoordX; currentSize++) {
+                frames.add(new ArrayList<Frame>());
+            }
+        }
+
+        List<Frame> column = frames.get(frameCoordX);
+        // add new frames if needed
+        if (column.size() <= frameCoordY) {
+            for (int currentSize = column.size(); currentSize <= frameCoordY; currentSize++) {
+                Frame frame = new Frame();
+                frame.setMap(this);
+                column.add(frame);
+            }
+        }
+
+        // set item position relative to frame
+        int itemOffsetX = item.getArea().getX() % Frame.FRAME_WIDTH;
+        int itemOffsetY = item.getArea().getY() % Frame.FRAME_HEIGHT;
+
+        item.getArea().setX(itemOffsetX);
+        item.getArea().setY(itemOffsetY);
+
+        // finally add item to the containing frame
+        Frame containingFrame = column.get(frameCoordY);
+        containingFrame.addItem(item);
     }
 
     /**
@@ -74,70 +75,70 @@ public class Map {
      */
     public Frame getNeighbour(Frame caller, DIRECTION direction) {
         // search for the given frame
-    	int callerColIndex = -2;
-    	int callerRowIndex = -2;
-    	
-    	SEARCH: for (int columnIndex = 0; columnIndex < frames.size(); columnIndex++) {
-    		List<Frame> column = frames.get(columnIndex);
-    		for (int rowIndex = 0; rowIndex < column.size(); rowIndex++) {
-    			if (column.get(rowIndex) == caller) {
-    				// caller found
-    				callerColIndex = columnIndex;
-    				callerRowIndex = rowIndex;
-    				break SEARCH;
-    			}
-    		}
-    	}
-    	
-    	// get neighbour index
-    	int neighbourColIndex = -1;
-    	int neighbourRowIndex = -1;
-    	
-    	switch (direction) {
-		case UP:
-			neighbourColIndex = callerColIndex;
-			neighbourRowIndex = callerRowIndex - 1;
-			break;
-			
-		case RIGHT:
-			neighbourColIndex = callerColIndex + 1;
-			neighbourRowIndex = callerRowIndex;
-			break;
-			
-		case DOWN:
-			neighbourColIndex = callerColIndex;
-			neighbourRowIndex = callerRowIndex + 1;
-			break;
-			
-		case LEFT:
-			neighbourColIndex = callerColIndex - 1;
-			neighbourRowIndex = callerRowIndex;
-			break;
+        int callerColIndex = -2;
+        int callerRowIndex = -2;
 
-		default:
-			break;
-		}
-    	
-    	//check for bounds
-    	if (neighbourColIndex < 0 || neighbourColIndex >= frames.size()) {
-    		// neighbour is out of horizontal index
-    		return null;
-    	}
-    	
-    	List<Frame> neighbourColumn = frames.get(neighbourColIndex);
-    	
-    	if (neighbourRowIndex < 0 || neighbourRowIndex >= neighbourColumn.size()) {
-    		// neighbour is out of vertical index
-    		return null;
-    	}
-    	
-    	Frame neighbour = neighbourColumn.get(callerRowIndex);
-    	
-    	if (caller.isTraversable(neighbour, direction)) {
-    		return neighbour;
-    	} else {
-    		return null;
-    	}
+        SEARCH: for (int columnIndex = 0; columnIndex < frames.size(); columnIndex++) {
+            List<Frame> column = frames.get(columnIndex);
+            for (int rowIndex = 0; rowIndex < column.size(); rowIndex++) {
+                if (column.get(rowIndex) == caller) {
+                    // caller found
+                    callerColIndex = columnIndex;
+                    callerRowIndex = rowIndex;
+                    break SEARCH;
+                }
+            }
+        }
+
+        // get neighbour index
+        int neighbourColIndex = -1;
+        int neighbourRowIndex = -1;
+
+        switch (direction) {
+        case UP:
+            neighbourColIndex = callerColIndex;
+            neighbourRowIndex = callerRowIndex - 1;
+            break;
+
+        case RIGHT:
+            neighbourColIndex = callerColIndex + 1;
+            neighbourRowIndex = callerRowIndex;
+            break;
+
+        case DOWN:
+            neighbourColIndex = callerColIndex;
+            neighbourRowIndex = callerRowIndex + 1;
+            break;
+
+        case LEFT:
+            neighbourColIndex = callerColIndex - 1;
+            neighbourRowIndex = callerRowIndex;
+            break;
+
+        default:
+            break;
+        }
+
+        //check for bounds
+        if (neighbourColIndex < 0 || neighbourColIndex >= frames.size()) {
+            // neighbour is out of horizontal index
+            return null;
+        }
+
+        List<Frame> neighbourColumn = frames.get(neighbourColIndex);
+
+        if (neighbourRowIndex < 0 || neighbourRowIndex >= neighbourColumn.size()) {
+            // neighbour is out of vertical index
+            return null;
+        }
+
+        Frame neighbour = neighbourColumn.get(callerRowIndex);
+
+        if (caller.isTraversable(neighbour, direction)) {
+            return neighbour;
+        } else {
+            return null;
+        }
     }
 
     /**
@@ -147,7 +148,91 @@ public class Map {
      * @param d Mozgazás iránya
      */
     public void moveFrame(DIRECTION d) {
+        // TODO implement moveFrame
         throw new UnsupportedOperationException();
     }
 
+    public FrameIterator frameIterator() {
+        return new FrameIterator();
+    }
+
+    public class FrameIterator implements Iterator<Frame> {
+
+        private int nextColumnIndex = 0;
+        private Iterator<Frame> currentColumnIterator = null;
+        private int currentRowIndex;
+
+        public FrameIterator() {
+            List<Frame> firstColumn = getNextColumn();
+
+            if (firstColumn != null) {
+                currentColumnIterator = firstColumn.iterator(); 
+            }
+        }
+
+        private List<Frame> getNextColumn() {
+            List<Frame> nextColumn = null;
+
+            if (frames.size() > nextColumnIndex) {
+                nextColumn = frames.get(nextColumnIndex);
+                nextColumnIndex++;
+                currentRowIndex = -1;
+            }
+
+            return nextColumn;
+        }
+
+        @Override
+        public boolean hasNext() {
+            if (currentColumnIterator.hasNext() == true) {
+                // current col is not finished yet
+                return true;
+            } else {
+                // current col finished, is there any other col?
+                List<Frame> nextColumn = getNextColumn();
+                if (nextColumn != null && nextColumn.size() > 0) {
+                    currentColumnIterator = nextColumn.iterator();
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+        }
+
+        @Override
+        public Frame next() {
+            currentRowIndex++;
+            return currentColumnIterator.next();
+        }
+
+        public Area getFrameOffset() {
+            Area offset = new Area();
+
+            offset.setX((nextColumnIndex-1) * Frame.FRAME_WIDTH);
+            offset.setY(currentRowIndex * Frame.FRAME_HEIGHT);
+
+            return offset;
+        }
+
+        @Override
+        public void remove() {
+            throw new UnsupportedOperationException();
+        }
+
+    }
+
+    public int horizontalFrameCount() {
+        return frames.size();
+    }
+
+    public int verticalFrameCount() {
+        int verticalFrameCount = 0;
+        for (List<Frame> column : frames) {
+            if (column.size() > verticalFrameCount) {
+                verticalFrameCount = column.size();
+            }
+        }
+
+        return verticalFrameCount;
+    }
 }
