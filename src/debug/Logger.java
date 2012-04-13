cd6eb8ebafa5010309f3c8d2cb3abeeb51f0b922
erenon <erenon2@gmail.com>
2012-04-13 19:11:42 +0200

    Logger: logMap added, uses buffering

--- src/debug/Logger.java
+++ src/debug/Logger.java
@@ -6,11 +6,43 @@ package debug;
  * @responsibility Hibakeresést elősegítő üzenetek megjelenítése
  */
 public class Logger {
+    
+    /**
+     * Status üzenetek puffere
+     */
+    static StringBuilder statusBuffer = new StringBuilder();
+    
+    /**
+     * Map üzenetek kimeneti puffere
+     */
+    static StringBuilder mapBuffer = new StringBuilder();
+    
     /**
      * Státusz üzenet megjelenítése
      * @param status Üzenet 
      */
 	public static void logStatus(String status) {
-		System.out.println(status);
+		//System.out.println(status);
+	    statusBuffer.append(status);
+	    statusBuffer.append("\n");
+	}
+	
+	/**
+	 * Pályakép megjelenítése
+	 * @param map
+	 */
+	public static void logMap(String map) {
+	    mapBuffer.append(map);
+	}
+	
+	/**
+	 * Kimenet ürítése
+	 */
+	public static void flush() {
+	    System.out.print(statusBuffer.toString());
+	    System.out.print(mapBuffer.toString());
+	    
+	    statusBuffer = new StringBuilder();
+	    mapBuffer = new StringBuilder();
 	}
 }
